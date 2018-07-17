6ea56bbce0c8a8e08857645b68b0e6362d61dd2b
nmittler <nmittler@gmail.com>
2018-07-16 17:12:14 -0700

    Adding e2e test for outbound listener conflicts

--- tests/e2e/tests/pilot/listener_conflict_test.go
+++ tests/e2e/tests/pilot/listener_conflict_test.go
@@ -21,10 +21,16 @@ import (
 	"testing"
 	"text/template"
 
+	"encoding/json"
+	"regexp"
+
+	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/tests/util"
 )
 
-var (
+const (
+	metricName              = "pilot_conflict_outbound_listener_tcp_over_current_tcp"
+	listenerName            = "0.0.0.0:15151"
 	conflictServiceTemplate = `
 apiVersion: v1
 kind: Namespace
@@ -50,59 +56,136 @@ spec:
 `
 )
 
+var (
+	messagePattern = regexp.MustCompile("^Listener=0.0.0.0:15151 AcceptedTCP=([0-9a-zA-Z-.]+) RejectedTCP=([0-9a-zA-Z-.]+) TCPServices=([0-9]+)$")
+)
+
+type serviceInfo struct {
+	ServiceName string
+	Namespace   string
+}
+
+func (i *serviceInfo) fullName() string {
+	return fmt.Sprintf("%s.%s.svc.cluster.local", i.ServiceName, i.Namespace)
+}
+
 func TestListenerConflicts(t *testing.T) {
 	if !tc.Kube.IsClusterWide() {
 		t.Skip("Cluster-wide deployment is required for this test so that pilot will observe all namespaces.")
 	}
 
 	// Deploy 2 headless TCP services with conflicting ports.
-	cases := []struct {
-		ServiceName string
-		Namespace   string
-	}{
-		{
-			ServiceName: fmt.Sprintf("service1"),
-			Namespace:   fmt.Sprintf("%s-conflict-1", tc.Info.RunID),
-		},
-		{
-			ServiceName: fmt.Sprintf("service2"),
-			Namespace:   fmt.Sprintf("%s-conflict-2", tc.Info.RunID),
-		},
+	oldService := &serviceInfo{
+		ServiceName: fmt.Sprintf("service1"),
+		Namespace:   fmt.Sprintf("%s-conflict-1", tc.Info.RunID),
 	}
-	for _, c := range cases {
+	newService := &serviceInfo{
+		ServiceName: fmt.Sprintf("service2"),
+		Namespace:   fmt.Sprintf("%s-conflict-2", tc.Info.RunID),
+	}
+	for _, c := range []*serviceInfo{oldService, newService} {
 		yaml := getConflictServiceYaml(c, t)
 		kubeApplyConflictService(yaml, t)
 		defer util.KubeDeleteContents("", yaml, tc.Kube.KubeConfig)
 	}
 
 	for cluster := range tc.Kube.Clusters {
-		testName := fmt.Sprintf("%s_%s", cases[0].ServiceName, cases[1].ServiceName)
+		testName := oldService.ServiceName
 		runRetriableTest(t, cluster, testName, 5, func() error {
 			// Scrape the pilot logs and verify that the newer service is filtered in favor of the older service.
 			logs := getPilotLogs(t)
-			lines := strings.Split(logs, "\n")
-			secondServiceFiltered := false
-			for _, line := range lines {
-				if strings.Contains(line, "omitting filterchain") {
+			pushStatuses, err := getPushStatus(logs)
+			if err != nil {
+				return err
+			}
 
-					// Nothing from the first service should be filtered.
-					if strings.Contains(line, cases[0].ServiceName) {
-						return fmt.Errorf("first headless TCP service unexpectedly filtered")
-					}
-
-					// Check if the second service was filtered.
-					if strings.Contains(line, cases[1].ServiceName) {
-						secondServiceFiltered = true
-					}
+			for _, pushStatus := range pushStatuses {
+				conflict, ok := pushStatus.ProxyStatus[metricName]
+				if !ok {
+					continue
 				}
+				event, ok := conflict[listenerName]
+				if !ok {
+					continue
+				}
+
+				accepted, rejected, numServices, err := parseEventMessageFields(event.Message)
+				if err != nil {
+					return err
+				}
+
+				if accepted == newService.fullName() {
+					// We should never accept the port from the newer service.
+					return fmt.Errorf("expected to reject listener for %s, but it was accepted", accepted)
+				}
+				if accepted != oldService.fullName() {
+					// Possibly a rejection for a different service? Just go to the next event.
+					continue
+				}
+
+				// The old service was accepted. The rejection should be the new service.
+				if rejected != newService.fullName() {
+					return fmt.Errorf("expected to reject listener for %s, but rejected %s", newService.fullName(), rejected)
+				}
+				if numServices != "1" {
+					return fmt.Errorf("expected 1 TCP services, but found %s", 1, numServices)
+				}
+
+				return nil
 			}
 
-			if !secondServiceFiltered {
-				return fmt.Errorf("second headless TCP service was not filtered")
-			}
-			return nil
+			return fmt.Errorf("failed to find ADS push status in pilot logs")
 		})
 	}
+}
+
+func parseEventMessageFields(message string) (accepted string, rejected string, numServices string, err error) {
+	matches := messagePattern.FindStringSubmatch(message)
+	if len(matches) != 4 {
+		err = fmt.Errorf("unexpected groups in message. Expected %d, found %d", 4, len(matches))
+		return
+	}
+
+	accepted = matches[1]
+	rejected = matches[2]
+	numServices = matches[3]
+	return
+}
+
+func getPushStatus(logs string) ([]*model.PushStatus, error) {
+	lines := strings.Split(logs, "\n")
+	statuses := make([]*model.PushStatus, 0)
+	for i := 0; i < len(lines); i++ {
+		line := lines[i]
+		if strings.Contains(line, "ads\tPush finished") {
+			// Gather the push status JSON.
+			pushStatusJSON := "{\n"
+			i++
+			for ; i < len(lines); i++ {
+				line = lines[i]
+				if strings.HasPrefix(line, "}") {
+					// Found the end of the json.
+					pushStatusJSON += "}"
+
+					// Parse the push status.
+					pushStatus := &model.PushStatus{}
+					if err := json.Unmarshal([]byte(pushStatusJSON), pushStatus); err != nil {
+						return nil, err
+					}
+					statuses = append(statuses, pushStatus)
+					break
+				}
+				// This line is part of the push status.
+				pushStatusJSON += line
+			}
+		}
+	}
+
+	if len(statuses) == 0 {
+		return nil, fmt.Errorf("no push status found in logs")
+	}
+
+	return statuses, nil
 }
 
 func getPilotLogs(t *testing.T) string {
